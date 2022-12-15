21d83c48fccfefffd2f3b0cdb544853d84402840
Berni <bernhard.schoeffel@gmail.com>
2022-12-15 19:05:26 +0100

    Day15 now Working

--- Day15/main.cpp
+++ Day15/main.cpp
@@ -89,12 +89,13 @@ vector<string>& seperateString(string toSeperate, const char *seperator) {
     return result;
 }
 
-void processData(const vector<string> &vecSingle, int &saveVar) {
-    int minX = 100000, maxX = 0;
-    int minY = 100000, maxY = 0;
+void processDataPartTwo(const vector<string> &vecSingle, int &saveVar) {
+    int minX = 100'000, maxX = 0;
+    int minY = 100'000, maxY = 0;
 
-    //int desiredRow = 2000000;
-    int desiredRowInt = 10;
+    //int desiredRowInt = 10;
+    int desiredRowInt = 3'999'000;
+    //int desiredRowInt = 10;
 
     vector<Sensor*> sensors;
 
@@ -113,44 +114,110 @@ void processData(const vector<string> &vecSingle, int &saveVar) {
         sensors.push_back(new Sensor(stoi(data[0]), stoi(data[1]), stoi(data[2]), stoi(data[3])));
     }
 
-    char desiredRowRow[maxX-minX];
-    for(int i = 0; i < (maxX-minX); i++) {
-        desiredRowRow[i] = '.';
-    }
+    cout << maxX << "," << maxY << "    " << minX << "," << minY << endl;
+    bool desiredRowRow[maxX-minX];
+    for(; desiredRowInt < 4'000'000; desiredRowInt++) {
 
+    for(long long y = 0; y < 1; y++) {
+        for(long long i = 0; i < (maxX-minX); i++) {
+            desiredRowRow[i] = false;
+        }
+    }
     for(auto s : sensors) {
         if(s->ownY > desiredRowInt) {
             //Hoch
             if((s->ownY - s->getDistance()) <= desiredRowInt) {
                 int dif = abs((s->ownY - s->getDistance()) - desiredRowInt);
-                for(int i = 0; i <= 2 * dif; i++) {
-                    desiredRowRow[(s->ownX-dif)+i-minX-1] = 'X';
-                    //cout << s->ownX << " | " << s->ownY << " -> " << s->getDistance() << endl;
+                for(int i = 0; i < (2 * dif); i++) {
+                    if(((s->ownX-dif)+i-minX-1) > 0 && ((s->ownX-dif)+i-minX-1) < 4'000'000) {
+                    desiredRowRow[(s->ownX-dif)+i-minX-1] = true;
+                    }
                 }
-                //cout << "Sensor" << endl;
             } 
         } else {
             //Runter
             if((s->ownY + s->getDistance()) >= desiredRowInt) {
                 int dif = (s->ownY + s->getDistance()) - desiredRowInt;
-                for(int i = 0; i <= 2 * dif; i++) {
-                    if(s->ownY==7 && s->ownX==8);
-                    desiredRowRow[(s->ownX-dif)+i-minX-1] = 'X';
+                for(int i = 0; i < (2 * dif); i++) {
+                    if(((s->ownX-dif)+i-minX-1) > 0 && ((s->ownX-dif)+i-minX-1) < 4'000'000) {
+                    desiredRowRow[(s->ownX-dif)+i-minX-1] = true;
+                    }
                 }
-                //cout << "Sensor" << endl;
+            }
+        }
+    }
+    }
+    for(int i = 0; i < (maxX-minX); i++) {
+        if(desiredRowRow[i] == false) {
+            cout << "POOOOOIIIINT" << i << "  " << desiredRowInt << endl;
+            break;;
+        }
+    }
+}
+
+void processData(const vector<string> &vecSingle, int &saveVar) {
+    int minX = 100'000, maxX = 0;
+    int minY = 100'000, maxY = 0;
+
+    //int desiredRowInt = 10;
+    int desiredRowInt = 2'000'000;
+    int rowOffset = 2'000'000;
+    //1'000'000;
+    //int desiredRowInt = 10;
+
+    vector<Sensor*> sensors;
+
+    for(int i = 0; i < vecSingle.size(); i++) {
+        vector<string> data = seperateString(vecSingle[i], "Sensor at closest beacon is x= y= : ,");
+        minX = min(minX, stoi(data[0]));
+        minX = min(minX, stoi(data[2]));
+        maxX = max(maxX, stoi(data[0]));
+        maxX = max(maxX, stoi(data[2]));
+        
+        minY = min(minY, stoi(data[1]));
+        minY = min(minY, stoi(data[3]));
+        maxY = max(maxY, stoi(data[1]));
+        maxY = max(maxY, stoi(data[3]));
+
+        sensors.push_back(new Sensor(stoi(data[0]), stoi(data[1]), stoi(data[2]), stoi(data[3])));
+    }
+
+    char desiredRowRow[maxX-minX+rowOffset];
+    for(long long i = 0; i < (maxX-minX+rowOffset); i++) {
+        desiredRowRow[i] = '.';
+    }
+
+    cout << maxX << "," << maxY << "    " << minX << "," << minY << endl;
+
+    for(auto s : sensors) {
+        cout << s->getDistance() << endl;
+        if(s->ownY > desiredRowInt) {
+            //Hoch
+            if((s->ownY - s->getDistance()) <= desiredRowInt) {
+                int dif = abs((s->ownY - s->getDistance()) - desiredRowInt);
+                for(int i = 0; i < (2 * dif); i++) {
+                    desiredRowRow[(s->ownX-dif)+i-minX-1+rowOffset/2] = 'X';
+                }
+            } 
+        } else {
+            //Runter
+            if((s->ownY + s->getDistance()) >= desiredRowInt) {
+                int dif = (s->ownY + s->getDistance()) - desiredRowInt;
+                for(int i = 0; i < (2 * dif); i++) {
+                    desiredRowRow[(s->ownX-dif)+i-minX-1+rowOffset/2] = 'X';
+                }
             }
         }
     }
 
     
-    for(int i = 0; i < (maxX-minX); i++) {
-        if(!(i%5)) cout << "    ";
+    for(long long i = 0; i < (maxX-minX+rowOffset); i++) {
+        //if(!(i%5)) cout << "    ";
         //cout << desiredRowRow[i];
         if(desiredRowRow[i] == 'X') saveVar++;
     }
     //cout << endl;
 
-    cout << maxX << "," << maxY << "    " << minX << "," << minY << endl;
 
 }
 
@@ -159,11 +226,12 @@ int main() {
 
     //printLines(linesBernhard);
     //split2DimVec(linesBernhard);
-    processData(split2DimVec(linesBernhard), sumBernhard);
+    //processData(split2DimVec(linesBernhard), sumBernhard);
+    processDataPartTwo(split2DimVec(linesBernhard), sumBernhardPart2);
 
     readInData("jasmina.txt", linesJasmina);
     //printLines(linesJasmina);
-    processData(split2DimVec(linesJasmina), sumJasmina);
+    //processData(split2DimVec(linesJasmina), sumJasmina);
     
     cout << endl << "Bernhard: " << sumBernhard <<  "   " << sumBernhardPart2 << endl;
     cout << "Jasmina: " << sumJasmina << "  " << sumJasminaPart2 << endl;
